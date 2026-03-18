b6c9d56ca90f41374a1cee86f54050d4a81e8490
badgotidheeraj <badgotidheeraj@gmail.com>
2026-03-18 08:59:33 +0530

    create

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://hrmsbackend-ij0q.onrender.com/api";
 const EMPLOYEE_API_URL = `${API_BASE_URL}/employee-register/`;
 const ATTENDANCE_API_URL = `${API_BASE_URL}/attendance/`;
 const attendanceStorageKey = "hrms-attendance-records";
